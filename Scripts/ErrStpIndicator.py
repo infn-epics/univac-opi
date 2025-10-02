0aeda90d14ea97288122ad2999f5e7552471c101
pompilir <riccardo.pompili@lnf.infn.it>
2025-10-02 11:30:07 +0200

    updated status in vac_channel

--- Scripts/ErrStpIndicator.py
+++ Scripts/ErrStpIndicator.py
@@ -1,4 +1,5 @@
 from org.csstudio.display.builder.runtime.script import PVUtil, ScriptUtil
+from org.csstudio.display.builder.model.properties import WidgetColor
 
 #Get info from macros
 prefx = widget.getEffectiveMacros().getValue("P")
@@ -10,9 +11,19 @@ if 'VGA' in mydev:
     if 'No error' in pv_value:
         pv_value="OK"
     else:
-        pv_value="ERR"
+        pv_value="NOK"
 else:
-    pvName=PVUtil.createPV(prefx+":"+mydev+":STPTSTAT_RB",100)
-    pv_value=PVUtil.getString(pvName)
+    pvName1=PVUtil.createPV(prefx+":"+mydev+":STPTSTAT_RB",100)
+    pv_value1=PVUtil.getString(pvName1)
+    pvName2=PVUtil.createPV(prefx+":"+mydev+":STATUS_RB",100)
+    pv_value2=PVUtil.getString(pvName2)
+    if 'ON' in pv_value1 and 'ON' in pv_value2:
+        pv_value="OK"
+    else:
+        pv_value="NOK"
 
 widget.setPropertyValue('text',pv_value)
+if 'NOK' in pv_value:
+    widget.setPropertyValue("background_color", WidgetColor(255, 0, 0)) #red
+else:
+    widget.setPropertyValue("background_color", WidgetColor(0, 255, 0)) #green
